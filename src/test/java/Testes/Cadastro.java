c6e0db42820f7442c6ee63015785c661a1383448
ViniciusCanaan <60820733+ViniciusCanaan@users.noreply.github.com>
2020-02-08 16:23:20 -0300

    Versao 1

---
+++ src/test/java/Testes/Cadastro.java
@@ -0,0 +1,35 @@
+package Testes;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
+public class Cadastro {
+
+    @Test
+    public void testFazerCadastro(){ //criando um método para fazer login na página
+
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Driver\\chromedriver.exe"); //Buscando pelo meu arquivo Webdriver
+        WebDriver navegador = new ChromeDriver(); // esta linha irá abrir meu browser
+        navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        navegador.get("https://neon.sambaplay.tv/"); // solicitando que meu webdriver acesse a página da sambaplay.tv
+        navegador.findElement(By.id("header-sign-in-btn")).click(); //procurando pelo elemento com o id "header-sign-in-btn" e executando a função de click;
+        navegador.findElement(By.id("register-button-login-page")).click(); //procurando pelo elemento com o id "register-button-login-page" e executando a função de click de registro.
+        navegador.findElement(By.name("name")).sendKeys("Vinícius Canaan");
+        navegador.findElement(By.id("registerEmail")).sendKeys("vcfreitas1997@gmail.com");
+        navegador.findElement(By.id("registerConfEmail")).sendKeys("vcfreitas1997@gmail.com");
+        navegador.findElement(By.id("registerPassword")).sendKeys("12345678");
+        navegador.findElement(By.id("registerConfPassword")).sendKeys("12345678");
+
+
+        ChromeOptions options = new ChromeOptions();
+
+
+    }
+}
---
+++ src/test/java/Testes/TesteGratis.java
@@ -0,0 +1,37 @@
+package Testes;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class TesteGratis {
+
+        @Test
+        public void testTesteGratis(){ //criando um método para fazer login na página
+
+            System.setProperty("webdriver.chrome.driver","C:\\Users\\Driver\\chromedriver.exe"); //Buscando pelo meu arquivo Webdriver
+            WebDriver navegador = new ChromeDriver(); // esta linha irá abrir meu browser
+            navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+            navegador.get("https://neon.sambaplay.tv/"); // solicitando que meu webdriver acesse a página da sambaplay.tv
+            navegador.findElement(By.id("header-sign-in-btn")).click(); //procurando pelo elemento com o id "header-sign-in-btn" e executando a função de click;
+            navegador.findElement(By.id("loginEmail")).sendKeys("vcfreitas1997@gmail.com");
+            navegador.findElement(By.id("loginPassword")).sendKeys("12345678");
+            navegador.findElement(By.id("formLogin"));
+            navegador.findElement(By.id("btn-login")).click();
+            navegador.findElement(By.linkText("Conteúdo")).click();
+            navegador.findElement(By.id("cta_subscription_trial")).click();
+            navegador.findElement(By.id("menuBoleto")).click();
+            navegador.findElement(By.id("cpf")).sendKeys("21167297504");
+            navegador.findElement(By.id("btn-finish-payment-boleto")).click();
+            ChromeOptions options = new ChromeOptions();
+
+
+        }
+}
